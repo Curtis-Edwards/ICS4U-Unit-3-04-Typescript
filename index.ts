3cd21914d94bd6fd87b5441065eecbfd49383a6d
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-05-02 17:51:45 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -10,10 +10,10 @@ import { createPrompt } from 'bun-promptx'
 
 function hanoi(ndisks: number, startPeg: number, endPeg: number) {
     if (ndisks == 1) {
-        console.log(`Move disk 1 from peg ${startpeg} to peg ${endpeg}`)
+        console.log(`Move disk 1 from peg ${startPeg} to peg ${endPeg}`)
     } else {
         hanoi(ndisks - 1, startPeg, 6 - startPeg - endPeg)
-        console.log(`Move disk ${ndisks} from peg ${startpeg} to peg ${endPeg}`)
+        console.log(`Move disk ${ndisks} from peg ${startPeg} to peg ${endPeg}`)
         hanoi(ndisks - 1, 6 - startPeg - endPeg, endPeg)
     }
 }
